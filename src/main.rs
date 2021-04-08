15b693a9813dd92a1fdbd58ec0bb2d5b6855f6c9
Peter Malmgren <ptmalmgren@gmail.com>
2021-04-08 14:50:03 -0400

    Refactor code to use a broadcast channel

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,16 @@ extern crate notify;
 
 use std::error;
 use std::io::Write;
-use std::sync::mpsc::channel;
+use std::sync::mpsc::{channel, RecvTimeoutError};
+use std::time::{Duration, Instant};
 
-use structopt::StructOpt;
-use notify::{raw_watcher, RawEvent, RecursiveMode, Watcher};
-use tokio::sync::mpsc;
 use chrono::Local;
 use env_logger::Builder;
 use log::LevelFilter;
+use notify::{raw_watcher, RawEvent, RecursiveMode, Watcher};
+use structopt::StructOpt;
+use tokio::sync::broadcast;
+use tokio::net::{TcpListener, TcpStream};
 
 #[derive(StructOpt)]
 struct Cli {
@@ -17,6 +19,7 @@ struct Cli {
     file_path: Option<std::path::PathBuf>,
 }
 
+#[derive(Clone, Debug)]
 enum FileChangeOperation {
     Chmod = 1,
     CloseWrite,
@@ -28,6 +31,7 @@ enum FileChangeOperation {
     Unknown,
 }
 
+#[derive(Clone, Debug)]
 struct FileChange {
     path: String,
     operation: FileChangeOperation,
@@ -57,6 +61,62 @@ impl FileChange {
     }
 }
 
+async fn spawn_filewatcher(tx: broadcast::Sender<Vec<FileChange>>, file_path: String) -> Result<(), Box<dyn error::Error>> {
+    tokio::spawn(async move {
+        let (watcher_tx, watcher_rx) = channel();
+        // let mut watcher = raw_watcher(watcher_tx).map_err(|e| e.to_string())?;
+        let mut watcher = raw_watcher(watcher_tx).expect("Error: Initializing Raw Watcher");
+        // watcher.watch(file_path, RecursiveMode::Recursive).map_err(|e| e.to_string())?;
+        watcher
+            .watch(file_path, RecursiveMode::Recursive)
+            .expect("Error: Watcher");
+        log::info!("File watcher started...");
+
+        let mut changes: Vec<FileChange> = vec!();
+        let mut last_change: Option<Instant> = None;
+
+        loop {
+            match watcher_rx.recv_timeout(Duration::from_millis(250)) {
+                Ok(RawEvent {
+                    path: Some(path),
+                    op: Ok(op),
+                    cookie: _cookie,
+                }) => changes.push(FileChange::new(op, path.to_str().unwrap().to_string())),
+                Ok(event) => log::error!("broken event: {:?}", event),
+                Err(e) => match e {
+                    RecvTimeoutError::Timeout => (),
+                    RecvTimeoutError::Disconnected => {
+                        log::error!("Notify disconnected");
+                        break;
+                    },
+                }
+            };
+
+            if changes.len() > 0 {
+                let should_send = match last_change {
+                    Some(t) => Instant::now().duration_since(t) > Duration::from_secs(1),
+                    None => {
+                        last_change = Some(Instant::now());
+                        false
+                    },
+                };
+                if !should_send {
+                    continue;
+                }
+
+                if let Err(e) = tx.send(changes) {
+                    log::error!("Error sending changes: {:?}", e);
+                }
+                changes = vec!();
+                last_change = None;
+            }
+        }
+    });
+
+
+    Ok(())
+}
+
 async fn run() -> Result<(), Box<dyn error::Error>> {
     let args = Cli::from_args();
 
@@ -65,36 +125,17 @@ async fn run() -> Result<(), Box<dyn error::Error>> {
         None => ".".to_string(),
     };
 
-    let (tx, mut rx) = mpsc::channel(32);
+    let (tx, mut rx): (broadcast::Sender<Vec<FileChange>>, broadcast::Receiver<Vec<FileChange>>) = broadcast::channel(10);
 
-    tokio::spawn(async move {
-        let (watcher_tx, watcher_rx) = channel();
-        // let mut watcher = raw_watcher(watcher_tx).map_err(|e| e.to_string())?;
-        let mut watcher = raw_watcher(watcher_tx).expect("Error: Initializing Raw Watcher");
-        // watcher.watch(file_path, RecursiveMode::Recursive).map_err(|e| e.to_string())?;
-        watcher.watch(file_path, RecursiveMode::Recursive).expect("Error: Watcher");
-        log::info!("File watcher started...");
+    // spawns a background task to watch for file changes
+    spawn_filewatcher(tx, file_path).await?;
 
-        loop {
-            let msg = match watcher_rx.recv() {
-                Ok(RawEvent {
-                    path: Some(path),
-                    op: Ok(op),
-                    cookie,
-                }) => {
-                    format!("{:?} {:?} ({:?})", op, path, cookie)
-                }
-                Ok(event) => format!("broken event: {:?}", event),
-                Err(e) => format!("watch error: {:?}", e),
-            };
-            tx.send(msg).await;
+    while let Ok(file_changes) = rx.recv().await {
+        for file_change in file_changes {
+            log::info!("Path: {} Operation: {:?}", file_change.path, file_change.operation);
         }
-    });
-
-    while let Some(message) = rx.recv().await {
-        log::info!("Got = {}", message);
     }
-    
+
     Ok(())
 }
 
@@ -112,7 +153,7 @@ pub async fn main() {
         })
         .filter(None, LevelFilter::Info)
         .init();
-    
+
     if let Err(e) = run().await {
         log::error!("Error = {}", e);
     }
